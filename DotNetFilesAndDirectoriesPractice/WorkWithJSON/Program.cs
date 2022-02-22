fb7c6621b31c81151284eb884c00d0ad528fa790
rifatislamrakesh <rifatislamrakesh@gmail.com>
2022-02-22 20:23:15 +0600

    Added some practice code for working with json object and file read write

---
+++ DotNetFilesAndDirectoriesPractice/WorkWithJSON/Program.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+using static SalesTotal;
+String salesJson = File.ReadAllText("sales.json");
+var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+
+Console.WriteLine(salesData.Total);
+
+File.WriteAllText("totals.txt", salesData.Total.ToString());
+
+//File.AppendAllText("totals.txt", $"{salesData.Total}{Environment.NewLine}");
---
+++ DotNetFilesAndDirectoriesPractice/WorkWithJSON/SalesTotal.cs
@@ -0,0 +1,4 @@
+public class SalesTotal
+{
+  public double Total { get; set; }
+}
