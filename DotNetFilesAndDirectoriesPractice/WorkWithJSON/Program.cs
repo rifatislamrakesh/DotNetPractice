d8a96631652da1cc12ad6391661080a776b0a3e8
rifatislamrakesh <rifatislamrakesh@gmail.com>
2022-02-24 17:30:31 +0600

    fixed

--- DotNetFilesAndDirectoriesPractice/WorkWithJSON/Program.cs
+++ DotNetFilesAndDirectoriesPractice/WorkWithJSON/Program.cs
@@ -3,8 +3,8 @@ using static SalesTotal;
 String salesJson = File.ReadAllText("sales.json");
 var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
 
-Console.WriteLine(salesData.Total);
+Console.WriteLine(salesData?.Total);
 
-File.WriteAllText("totals.txt", salesData.Total.ToString());
+File.WriteAllText("totals.txt", salesData?.Total.ToString());
 
 //File.AppendAllText("totals.txt", $"{salesData.Total}{Environment.NewLine}");
